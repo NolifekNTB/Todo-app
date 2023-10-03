19d7d6329e5c0d1cdd90ea6bb3f1be56958c4ea5
NolifekNTB <kacperkotlowskikk1@gmail.com>
2023-10-03 19:09:06 +0200

    Adding new tasks

--- app/src/main/java/com/example/todo_app/MainActivity.kt
+++ app/src/main/java/com/example/todo_app/MainActivity.kt
@@ -9,18 +9,22 @@ import layout.TaskAdapter
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private var taskList = mutableListOf<Task>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //recyclerView
         val recyclerView = binding.rv
-        val taskList = mutableListOf<Task>()
-        taskList.add(Task("Tytuł12", "opis1", false))
-        taskList.add(Task("Tytuł22", "opis2", true))
-        taskList.add(Task("Tytuł32", "opis3", false))
         val adapter = TaskAdapter(taskList)
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(this)
+
+        binding.btnAdd.setOnClickListener{
+            var textToAdd = binding.etAdd.text.toString()
+            taskList.add(Task(textToAdd, "opis", false))
+            adapter.notifyItemInserted(taskList.size - 1)
+        }
     }
 }
