1798274f84305aa93f262d327731b1b6963956c9
NolifekNTB <kacperkotlowskikk1@gmail.com>
2023-10-11 16:06:33 +0200

    Improve reading of code, after MVVM (2)

--- app/src/main/java/com/example/todo_app/VIew/MainActivity.kt
+++ app/src/main/java/com/example/todo_app/VIew/MainActivity.kt
@@ -1,24 +1,19 @@
 package com.example.todo_app.VIew
 
-import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.todo_app.databinding.ActivityMainBinding
-import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
 import com.example.todo_app.Model.Task
-import java.io.File
-import java.io.IOException
-import android.view.View
-import android.widget.AdapterView
-
-
-
+import androidx.recyclerview.widget.RecyclerView
+import com.example.todo_app.ViewModel.TaskViewModel
 class MainActivity : AppCompatActivity() {
+    private val mainVM = TaskViewModel()
     private lateinit var binding: ActivityMainBinding
     private var taskList = mutableListOf<Task>()
+    private lateinit var adapter: TaskAdapter
+    private lateinit var recyclerView: RecyclerView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,101 +21,30 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         //Read list
-        var x = FileHelper.readListFromFile(this, "daneListy")
+        taskList = mainVM.readListFromFile(this, "daneListy").sortedWith(compareByDescending {!it.status }).toMutableList()
 
-        var uncheckedTasks: List<Task> = emptyList()
-        var checkedTasks:List<Task> = emptyList()
-        for (i in 0..x.size-1){
-            uncheckedTasks = x.filter { !it.status } // Zadania nieodznaczone
-            checkedTasks = x.filter { it.status } // Zadania zaznaczone
-        }
-        taskList.addAll(uncheckedTasks) // Najpierw dodaj nieodznaczone zadania
-        taskList.addAll(checkedTasks) // Następnie dodaj zaznaczone zadania
-
-        //recyclerView
-        val recyclerView = binding.rv
-        val adapter = TaskAdapter(taskList)
+        //RecyclerView setup
+        recyclerView = binding.rv
+        adapter = TaskAdapter(taskList)
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(this)
 
         binding.btnAdd.setOnClickListener{
-            //walidacja daych
+            //Validation
             if (binding.etAdd.text.isEmpty()) {
                 binding.etAdd.hint = "To pole nie może być puste"
             } else {
-                //add task
-                var textToAdd = binding.etAdd.text.toString()
+                // Add task
+                val textToAdd = binding.etAdd.text.toString()
                 taskList.add(Task(textToAdd, "opis", false))
                 adapter.notifyItemInserted(taskList.size - 1)
                 binding.etAdd.setText("")
             }
         }
     }
-
-    override fun onResume() {
-        super.onResume()
-        val sortOptions = listOf("Nazwa", "Data", "Ocena")
-        binding.sortSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                val selectedItem = sortOptions[position]
-
-                // Tutaj obsłuż wybór użytkownika i wykonaj odpowiednie sortowanie
-                when (selectedItem) {
-                    "Nazwa" -> sortListByName()
-                    "Data" -> Log.d("myApp", "Data")
-                    "Ocena" -> Log.d("myApp", "Ocena")
-                }
-            }
-
-            override fun onNothingSelected(parent: AdapterView<*>?) {
-                // Obsłuż brak wyboru, jeśli to konieczne
-            }
-        }
-    }
-
-    fun sortListByName() {
-        taskList.sortBy { it.title }
-        //recyclerView
-        val recyclerView = binding.rv
-        val adapter = TaskAdapter(taskList)
-        recyclerView.adapter = adapter
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        adapter.notifyDataSetChanged()
-    }
-
     override fun onPause() {
         super.onPause()
         //Save list
-        FileHelper.saveListToFile(this, taskList, "daneListy")
-    }
-    object FileHelper {
-        private const val FILE_EXTENSION = ".json"
-
-        fun saveListToFile(context: Context, list: List<Task>, fileName: String) {
-            try {
-                val file = File(context.filesDir, fileName + FILE_EXTENSION)
-                val gson = Gson()
-                val jsonString = gson.toJson(list)
-                file.writeText(jsonString)
-            } catch (e: IOException) {
-                e.printStackTrace()
-            }
-        }
-
-        fun readListFromFile(context: Context, fileName: String): List<Task> {
-            try {
-                val file = File(context.filesDir, fileName + FILE_EXTENSION)
-                if (!file.exists()) {
-                    return emptyList()
-                }
-                val gson = Gson()
-                val jsonString = file.readText()
-                val listType = object : TypeToken<List<Task>>() {}.type
-                return gson.fromJson(jsonString, listType)
-            } catch (e: IOException) {
-                e.printStackTrace()
-            }
-            return emptyList()
-        }
+        mainVM.saveListToFile(this, taskList, "daneListy")
     }
 }
--- app/src/main/java/com/example/todo_app/ViewModel/TaskViewModel.kt
+++ app/src/main/java/com/example/todo_app/ViewModel/TaskViewModel.kt
@@ -1,7 +1,39 @@
 package com.example.todo_app.ViewModel
 
+import android.content.Context
 import androidx.lifecycle.ViewModel
+import com.example.todo_app.Model.Task
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import java.io.File
+import java.io.IOException
 
 class TaskViewModel: ViewModel() {
-
+    fun saveListToFile(context: Context, list: List<Task>, fileName: String) {
+        val FILE_EXTENSION = ".json"
+        try {
+            val file = File(context.filesDir, fileName + FILE_EXTENSION)
+            val gson = Gson()
+            val jsonString = gson.toJson(list)
+            file.writeText(jsonString)
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
+    fun readListFromFile(context: Context, fileName: String): List<Task> {
+        val FILE_EXTENSION = ".json"
+        try {
+            val file = File(context.filesDir, fileName + FILE_EXTENSION)
+            if (!file.exists()) {
+                return emptyList()
+            }
+            val gson = Gson()
+            val jsonString = file.readText()
+            val listType = object : TypeToken<List<Task>>() {}.type
+            return gson.fromJson(jsonString, listType)
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+        return emptyList()
+    }
 }
